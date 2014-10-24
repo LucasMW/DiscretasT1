3cca58b43b9da86a5100fd2ec4bb09e85a2ce420
LucasMW <great-lucas@hotmail.com>
2014-10-24 15:48:11 -0200

    Create gerapalavra.c
    It Works

---
+++ gerapalavra.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define WORDSIZE 3
+void GeraPalavras(int i,char* p,int n)
+{
+	int j;
+	char c;
+	if(i==0)
+	{
+		printf("%s\t",p);
+	}
+	else
+	{
+		for(j=0;j<('z'-'a'+1);j++)
+		{
+			p[n-i]='a'+j;
+			GeraPalavras(i-1,p,n);
+		}
+	}
+}
+
+int main (void)
+{
+	char* p;
+	p=(char*)malloc(2);
+	p[WORDSIZE]=0; //null
+	GeraPalavras(WORDSIZE,p,WORDSIZE);
+	return 0;
+}
